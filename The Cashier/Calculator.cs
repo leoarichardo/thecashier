a181f533d9b67bd023af8b68b70055eae37115d6
leoarichardo <leo.richardo@students.amikom.ac.id>
2020-10-22 12:38:08 +0700

    Add project files.

---
+++ The Cashier/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Cashier
+{
+    class Calculator
+    {
+        private List<item> listitem;
+        private double total = 0;
+
+        public Calculator()
+        {
+            this.listitem = new List<item>();
+        }
+        public void addItem(item item)
+        {
+            this.listitem.Add(item);
+            this.total += item.getsubTotal();
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+        public List<item> getListItem()
+        {
+            return listitem;
+        }
+        
+    }
+}
